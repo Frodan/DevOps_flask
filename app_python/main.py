6f2f0198ba608ddad36097ad1333dba6de6bf1eb
frodan <daniil.fronc@mail.ru>
2021-09-29 17:33:36 +0300

    ! наговнокодил копипастой

--- app_python/main.py
+++ app_python/main.py
@@ -15,19 +15,24 @@ except:
     username = "test"
     password = "test"
 
+users = {
+    username: password
+}
 unused_wallets = []
-used_wallets = []
+used_wallets = {}
 
 
 @auth.verify_password
 def verify_password(user, passw):
-    if user == username and passw == password:
+    if users.get(user) == passw:
         return username
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 @auth.login_required
 def upload():
+    if auth.username() != username:
+        return "Not an admin"
     if request.method == 'POST':
         file = request.files['file']
         if file.filename == '':
@@ -39,7 +44,7 @@ def upload():
             with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'r') as wallets_file:
                 global unused_wallets, used_wallets
                 unused_wallets = [x.strip() for x in wallets_file.readlines()]
-                used_wallets = []
+                used_wallets = {}
             return redirect('/stats')
     else:
         return '''
@@ -52,15 +57,49 @@ def upload():
     </form>
     '''
 
+@app.route('/upload_users', methods=['GET', 'POST'])
+@auth.login_required
+def upload_users():
+    if auth.username() != username:
+        return "Not an admin"
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        else:
+            filename = 'users.csv'
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'r') as users_file:
+                global users
+                users = {
+                    username: password
+                }
+                for user in users_file.readlines():
+                    print(user)
+                    login, passw = [x.strip() for x in user.split(',')]
+                    users[login]= passw
+            return redirect('/stats')
+    else:
+        return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route('/get_wallet', methods=['GET'])
+@auth.login_required
 def get_wallet():
     if not unused_wallets:
         return "No wallets"
     else:
         print(unused_wallets)
         wallet = unused_wallets.pop()
-        used_wallets.append(wallet)
+        used_wallets[wallet] = auth.username()
         return f'''
         <!doctype html>
         <title>Wallet</title>
@@ -85,12 +124,16 @@ def get_wallet():
 @app.route('/stats', methods=['GET'])
 @auth.login_required
 def get_stats():
+    if auth.username() != username:
+        return "Not an admin"
+    wallets_to_print = [f"{x} : {used_wallets[x]}" for x in used_wallets.keys()]
     return f"""
+        Users" {users}<br>
         Used wallets: {len(used_wallets)}<br>
         Unused wallets: {len(unused_wallets)}<br>
         <br>
         Used:<br> 
-        {"<br>".join(used_wallets)}
+        {"<br><br>".join(wallets_to_print)}
     """
 
 
